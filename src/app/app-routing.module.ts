447ba2ae79f352f95b7da8af094e397108279fcb
Mihir <mihirhero2008@gmail.com>
2018-09-11 12:37:58 +0530

    routing setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { SelectBadgeComponent } from './select-badge/select-badge.component';
+import { CustomizeComponent } from './customize/customize.component';
+import { SelectProductsComponent } from './select-products/select-products.component';
+import { RemoveBadgesComponent } from './remove-badges/remove-badges.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SelectBadgeComponent
+  },
+  {
+    path: 'customize',
+    component: CustomizeComponent
+  },
+  {
+    path: 'products',
+    component: SelectProductsComponent
+  },
+    {
+    path: 'remove',
+    component: RemoveBadgesComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
