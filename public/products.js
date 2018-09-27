b8f14f280d7db3f21f89a1fa1b1ec09ab37a886a
Mihir <mihirhero2008@gmail.com>
2018-09-27 16:18:26 +0530

    pull

--- public/products.js
+++ public/products.js
@@ -44,6 +44,6 @@ function onLoad() {
         }
     };
 
-    xhttp.open("GET", "https://209f9b2b.ngrok.io/product", true);
+    xhttp.open("GET", "https://6618b4a8.ngrok.io/product", true);
     xhttp.send();
 }
