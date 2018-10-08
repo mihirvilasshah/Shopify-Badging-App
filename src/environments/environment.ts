cdd7b084fddee345c3055890fd231646e6838532
mihirvilasshah <mihirhero2008@gmail.com>
2018-10-08 12:11:21 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrl: 'http://172.16.18.189:8080/shopify',
 };
 
 /*
