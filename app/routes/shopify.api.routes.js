03360378c444cef0e46a02a76b3e53df6f2c8ca4
saiKrishnaAnumu <41675094+saiKrishnaAnumu@users.noreply.github.com>
2018-10-09 19:31:22 +0530

    Update shopify.api.routes.js

--- app/routes/shopify.api.routes.js
+++ app/routes/shopify.api.routes.js
@@ -61,6 +61,7 @@ module.exports = (app) => {
 
 
     app.get('/selectedBadgeID/:id', controller.selectedBadgeID);
+    app.get('/getSrc/:pid', controller.getSrc);
 
     app.get('/getProductPriceRange/:p1/:p2/:pr', controller.getProductPriceRange);
     // app.get('/getProductPriceRange/:query', controller.getProductPriceRange);
