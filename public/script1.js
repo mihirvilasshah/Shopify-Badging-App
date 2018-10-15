24fed6488534e62b906cbea370d469baf94066c2
mihirvilasshah <mihirhero2008@gmail.com>
2018-10-15 10:02:46 +0530

    badge opacity

--- public/script1.js
+++ public/script1.js
@@ -69,9 +69,9 @@ aren’t available to the outside world. */
               
                var link = forwardingAddress+"/picture/"+ data.Bid; 
                $('.product-single__photo-wrapper').eq(0).css('position', 'relative');
-               $('.product-single__photo-wrapper').eq(0).css('opacity', data.opvalue);
+               
                console.log(data.opvalue);
-               $('.product-single__photo-wrapper').eq(0).append('<img src="' + link + '" class="tricon-badge" alt="Badge" height="60" width="60" style="position:absolute ; top :'+ y +'px; left:'+x +'px;">');            
+               $('.product-single__photo-wrapper').eq(0).append('<img src="' + link + '" class="tricon-badge" alt="Badge" height="60" width="60" style="position:absolute ; top :'+ y +'px; left:'+x +'px;opacity:'+data.opvalue+'">');            
             }           
           }
           
